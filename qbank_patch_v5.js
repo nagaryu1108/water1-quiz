af997a641bb59d3263198f6bc632e5b7b7ab1d7c
nagaryu1108 <156630555+nagaryu1108@users.noreply.github.com>
2026-09-04 07:13:00 +0900

    Load second gap-analysis question batch

--- qbank_patch_v5.js
+++ qbank_patch_v5.js
@@ -1,13 +1,14 @@
 (function(){
   /* Stable entry point: preserve IDs/history while layering quality patches. */
   if(document.readyState==='loading'){
-    document.write('<script src="./qbank_core_patch_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_detail_patch_v6.js?v=76"><\/script>');
-    document.write('<script src="./qbank_enrichment_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_detail_round1_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_detail_complete_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_gap_batch1_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_chem_v7.js?v=76"><\/script>');
-    document.write('<script src="./qbank_quality_audit_v7.js?v=76"><\/script>');
+    document.write('<script src="./qbank_core_patch_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_detail_patch_v6.js?v=77"><\/script>');
+    document.write('<script src="./qbank_enrichment_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_detail_round1_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_detail_complete_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_gap_batch1_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_gap_batch2_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_chem_v7.js?v=77"><\/script>');
+    document.write('<script src="./qbank_quality_audit_v7.js?v=77"><\/script>');
   }
 })();
