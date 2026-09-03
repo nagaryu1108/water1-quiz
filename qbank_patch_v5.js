565bfceece1279a472ca91b56ba74a5d10f990c8
nagaryu1108 <156630555+nagaryu1108@users.noreply.github.com>
2026-09-04 00:55:39 +0900

    Load chemical formula renderer in v7

--- qbank_patch_v5.js
+++ qbank_patch_v5.js
@@ -1,8 +1,9 @@
 (function(){
   /* This file is already loaded by index.html. Keep it as the stable entry point so learning-history and page wiring do not regress. */
   if(document.readyState==='loading'){
-    document.write('<script src="./qbank_core_patch_v7.js?v=72"><\/script>');
-    document.write('<script src="./qbank_detail_patch_v6.js?v=72"><\/script>');
-    document.write('<script src="./qbank_enrichment_v7.js?v=72"><\/script>');
+    document.write('<script src="./qbank_core_patch_v7.js?v=73"><\/script>');
+    document.write('<script src="./qbank_detail_patch_v6.js?v=73"><\/script>');
+    document.write('<script src="./qbank_enrichment_v7.js?v=73"><\/script>');
+    document.write('<script src="./qbank_chem_v7.js?v=73"><\/script>');
   }
 })();
