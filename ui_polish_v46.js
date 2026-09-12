83c6937f02b9852a053fead362faad59ba5671b7
nagaryu1108 <156630555+nagaryu1108@users.noreply.github.com>
2026-09-12 21:41:19 +0900

    Label learning-flow and formula-fix release as v52

--- ui_polish_v46.js
+++ ui_polish_v46.js
@@ -1,8 +1,8 @@
 (function(){
 'use strict';
-var RELEASE='v51';
-var RELEASE_DATE='2026-09-11';
-var BUILD_ID='v51-20260911-ui46d';
+var RELEASE='v52';
+var RELEASE_DATE='2026-09-12';
+var BUILD_ID='v52-20260912-ui46e';
 
 function ensureStyle(){
   if(document.getElementById('water1-ui-v46-style'))return;
