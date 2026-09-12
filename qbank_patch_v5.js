237d64ff1c372993803110d6494b488be525a7bf
nagaryu1108 <156630555+nagaryu1108@users.noreply.github.com>
2026-09-12 22:55:38 +0900

    Load v53 mobile flow-width fix

--- qbank_patch_v5.js
+++ qbank_patch_v5.js
@@ -54,6 +54,7 @@
     document.write('<script src="./qbank_learning_visuals_v52.js?v=149"><\/script>');
     document.write('<script src="./visual_aid_audit_v53.js?v=150"><\/script>');
     document.write('<script src="./qbank_learning_visuals_v53.js?v=150"><\/script>');
+    document.write('<script src="./qbank_learning_visuals_v53_mobilefix.js?v=151"><\/script>');
     document.write('<script src="./ui_polish_v46.js?v=150"><\/script>');
   }
 })();
